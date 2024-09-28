2d1fa44ff271464c8701f329be88eb436e7d1856
Boyd Sorratat <redthegx@gmail.com>
2024-09-28 12:13:34 +0900

    chore: remove calculate_scale_atlas

--- src/utils/common.rs
+++ src/utils/common.rs
@@ -11,24 +11,6 @@ pub fn snap_to_grid(pos: Vec3) -> Vec3 {
     )
 }
 
-/// Calculates the scale for a sprite based on its frame size from a TextureAtlas.
-pub fn calculate_scale_atlas(
-    texture_atlas_handle: &Handle<TextureAtlas>,
-    texture_atlases: &Res<Assets<TextureAtlas>>,
-) -> Vec3 {
-    if let Some(texture_atlas) = texture_atlases.get(texture_atlas_handle) {
-        // Assuming each frame should fit within GRID_SIZE * CHARACTER_SIZE_COEF
-        let frame_size = texture_atlas.size;
-        Vec3::new(
-            GRID_SIZE * CHARACTER_SIZE_COEF / frame_size.x,
-            GRID_SIZE / frame_size.y,
-            1.0,
-        )
-    } else {
-        Vec3::ONE // Default scale if TextureAtlas isn't loaded yet
-    }
-}
-
 pub fn calculate_scale(texture_handle: &Handle<Image>, images: &Assets<Image>) -> Vec3 {
     if let Some(texture) = images.get(texture_handle) {
         let width = texture.size().x;
