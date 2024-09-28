98f4cbf181f3f78714f0564226464e4d176528b8
Boyd Sorratat <redthegx@gmail.com>
2024-09-28 11:22:37 +0900

    chore: remove unused variables

--- src/resources/game_assets.rs
+++ src/resources/game_assets.rs
@@ -13,10 +13,7 @@ pub struct GameAssets {
 
 impl GameAssets {
     /// Loads all necessary game assets, including the monster sprite sheet.
-    pub fn new(
-        asset_server: &Res<AssetServer>,
-        texture_atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>,
-    ) -> Self {
+    pub fn new(asset_server: &Res<AssetServer>) -> Self {
         // Load individual images
         let background = asset_server.load("images/background/background_test.png");
         let player = asset_server.load("images/players/player_test.png");
@@ -25,10 +22,6 @@ impl GameAssets {
 
         // Load the monster sprite sheet image
         let monster_sprite_sheet = asset_server.load("images/monsters/monster_sprite_sheet.png");
-
-        // the sprite sheet has 2 sprites arranged in a row, and they are all 500px x 500px
-        let layout = TextureAtlasLayout::from_grid(UVec2::splat(500), 2, 1, None, None);
-        let _texture_atlas_layout = texture_atlas_layouts.add(layout);
 
         GameAssets {
             background,
