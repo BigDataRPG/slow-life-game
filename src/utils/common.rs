ae1a6527083778ef0bd5d84fad16f0aa89333836
Boyd Sorratat <redthegx@gmail.com>
2024-09-28 12:24:06 +0900

    chore: add type u32 to f32

--- src/utils/common.rs
+++ src/utils/common.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-pub const GRID_SIZE:  = 32.0;
+pub const GRID_SIZE: f32 = 32.0;
 pub const CHARACTER_SIZE_COEF: f32 = 1.5;
 
 pub fn snap_to_grid(pos: Vec3) -> Vec3 {
@@ -11,15 +11,23 @@ pub fn snap_to_grid(pos: Vec3) -> Vec3 {
     )
 }
 
-pub fn calculate_scale(texture_handle: &Handle<Image>, images: &Assets<Image>) -> Vec3 {
+pub fn calculate_scale(
+    texture_handle: &Handle<Image>,
+    images: &Assets<Image>,
+    frame_size: Option<Vec2>,
+) -> Vec3 {
     if let Some(texture) = images.get(texture_handle) {
-        let width = texture.size().x;
-        let height = texture.size().y;
-        // Calculate the desired scale based on your requirements
-        // For example, scale to 64x64 units
-        Vec3::new(GRID_SIZE / width, GRID_SIZE / height, 1.0)
+        let (width, height) = if let Some(frame) = frame_size {
+            (frame.x, frame.y)
+        } else {
+            (texture.size().x as f32, texture.size().y as f32)
+        };
+        Vec3::new(
+            GRID_SIZE * CHARACTER_SIZE_COEF / width,
+            GRID_SIZE * CHARACTER_SIZE_COEF / height,
+            1.0,
+        )
     } else {
         Vec3::ONE
     }
 }
-
